a3dd1216fbd5d3cd79850a9cbf64b3b193e334dd
mare2002 <marko.spremic@gmail.com>
2026-01-12 22:52:26 +0100

    Fixed the pc gen

--- rtl/frontend/pc_gen.sv
+++ rtl/frontend/pc_gen.sv
@@ -50,7 +50,7 @@ module pc_gen #(
   if (LEN5_MULTIPLE_ISSUES==1) begin: gen_single_pc_increase
     assign pc_increase = -1'b1;
   end else begin: gen_multiple_pc_increase
-    assign pc_increase = -{1'b1, pc_o[ILEN_BIT_SUFF+LEN5_MULTIPLE_ISSUES_BITS-1:ILEN_BIT_SUFF]};
+    assign pc_increase = -{1'b1, add_pc_base[ILEN_BIT_SUFF+LEN5_MULTIPLE_ISSUES_BITS-1:ILEN_BIT_SUFF]};
   end
   always_comb begin : tgt_addr_op_mux
     if (early_jump_valid_i && !(bu_res_valid_i && bu_res_i.mispredict)) begin
